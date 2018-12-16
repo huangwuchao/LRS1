6cee5d964fa1959e1486463a4271a5007e7a830c
457 <12454>
2018-12-16 16:23:19 +0800

    删除功能和编辑功能

--- NodePrroject/src/server.js
+++ NodePrroject/src/server.js
@@ -8,8 +8,8 @@
 //     request('https://www.layui.com/test/table/demo1.json',function(error,response,body){
 //             res.end(body);
 //     });
-// }).listen(3008,function(){
-//     console.log('服务器已在3008端口起飞')
+// }).listen(3108,function(){
+//     console.log('server is running on http://localhost:3108')
 // });
 
 const express = require('express');
@@ -62,7 +62,7 @@ app.get('/goodscategory',function(req,res){
         //     });
         // });
 
-        user.find().skip((page-1)*limit*1).limit(limit*1).toArray((err,result)=>{
+        user.find().sort({"id":1}).skip((page-1)*limit*1).limit(limit*1).toArray((err,result)=>{
             // console.log(result);
             let data;
             if(err){
@@ -95,60 +95,95 @@ app.get('/goodscategory',function(req,res){
 })
 
 //商品列表
-app.get('/goodslist',function(req,res){
+// app.get('/goodslist',function(req,res){
+//     MongoClient.connect('mongodb://localhost:27017',function(err,database){
+//         if(err) throw err;
+//         let db = database.db('win');
+//         let user = db.collection('goodslist');
+//         let count = 0;
+
+//         let page = req.query.page;
+//         let limit = req.query.limit;
+//         user.find().toArray(function(err2,result2){
+//             sum = result2.length;
+//             return sum;
+//         });
+//         // user.find({},{_id:0,class:1}).limit(12).toArray(function(err,result){
+//         //     res.send({
+//         //         code:0,
+//         //         data:result,
+//         //         msg:'商品列表',
+//         //         count:sum
+//         //     });
+//         // });
+
+//         user.find().skip((page-1)*limit*1).limit(limit*1).toArray((err,result)=>{
+//             // console.log(result);
+//             let data;
+//             if(err){
+//                 // console.log(666);
+//                 data={
+//                     'code':1,
+//                     'data':[],
+//                     'msg':'没有商品',
+//                     'count':pages
+//                 }
+//             }else{
+//                 data = {
+//                     'code':0,
+//                     'data':result,
+//                     'msg':'商品列表',
+//                     'count':sum
+//                 }
+                
+//             }
+
+//             res.send(data);
+//         });
+        
+        
+
+
+//         database.close();
+//     })
+
+// })
+//点击删除，根据ID删除数据库数据
+app.get('/shangchu',function(req,res){
+    let idx = req.query.id*1;
+    console.log(idx);
     MongoClient.connect('mongodb://localhost:27017',function(err,database){
         if(err) throw err;
-        let db = database.db('win');
-        let user = db.collection('goodslist');
-        let count = 0;
+        let db = database.db('h1809');
+        let user = db.collection('xiangmu');
+       user.deleteOne({id:idx},(err,result)=>{
+            console.log(result)
+       })
 
-        let page = req.query.page;
-        let limit = req.query.limit;
-        user.find().toArray(function(err2,result2){
-            sum = result2.length;
-            return sum;
-        });
-        // user.find({},{_id:0,class:1}).limit(12).toArray(function(err,result){
-        //     res.send({
-        //         code:0,
-        //         data:result,
-        //         msg:'商品列表',
-        //         count:sum
-        //     });
-        // });
-
-        user.find().skip((page-1)*limit*1).limit(limit*1).toArray((err,result)=>{
-            // console.log(result);
-            let data;
-            if(err){
-                // console.log(666);
-                data={
-                    'code':1,
-                    'data':[],
-                    'msg':'没有商品',
-                    'count':pages
-                }
-            }else{
-                data = {
-                    'code':0,
-                    'data':result,
-                    'msg':'商品列表',
-                    'count':sum
-                }
-                
-            }
-
-            res.send(data);
-        });
-        
-        
+    });
+});
 
 
-        database.close();
-    })
 
-})
+//点击编辑，根据传过来的值改变数据库的值
+app.get('/bianji',function(req,res){
+    let edit_id = req.query.id*1;
+    let edit_name = req.query.name;
+    let edit_time = req.query.time;
 
+    MongoClient.connect('mongodb://localhost:27017',function(err,database){
+        if(err) throw err;
+        let db = database.db('h1809');
+        let user = db.collection('xiangmu');
+        //console.log(edit_id,edit_name,edit_time);
+        user.update({
+            id:edit_id
+        },{$set:{prile:edit_name,joinTime:edit_time}},(err,result)=>{
+            console.log(result);
+        })
+
+    });
+});
 app.listen(3108,function(){
     console.log('server is running on http://localhost:3108')
 });
