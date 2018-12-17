3fec1b41a854e2a062b82de1a2bd99ce3a219105
457 <12454>
2018-12-17 20:18:54 +0800

    搜索和上下架

--- NodePrroject/src/server.js
+++ NodePrroject/src/server.js
@@ -40,59 +40,59 @@ app.use(express.static('./'));
 //路由
 // let urlencodedParser = bodyParser.urlencoded({ extended: false })
 // 商品分类
-app.get('/goodscategory',function(req,res){
-    MongoClient.connect('mongodb://localhost:27017',function(err,database){
-        if(err) throw err;
-        let db = database.db('h1809');
-        let user = db.collection('xiangmu');
-        let count = 0;
+// app.get('/goodscategory',function(req,res){
+//     MongoClient.connect('mongodb://localhost:27017',function(err,database){
+//         if(err) throw err;
+//         let db = database.db('h1809');
+//         let user = db.collection('xiangmu');
+//         let count = 0;
 
-        let page = req.query.page;
-        let limit = req.query.limit;
-        user.find().toArray(function(err2,result2){
-            sum = result2.length;
-            return sum;
-        });
-        // user.find({},{_id:0,class:1}).limit(12).toArray(function(err,result){
-        //     res.send({
-        //         code:0,
-        //         data:result,
-        //         msg:'商品列表',
-        //         count:sum
-        //     });
-        // });
+//         let page = req.query.page;
+//         let limit = req.query.limit;
+//         user.find().toArray(function(err2,result2){
+//             sum = result2.length;
+//             return sum;
+//         });
+//         // user.find({},{_id:0,class:1}).limit(12).toArray(function(err,result){
+//         //     res.send({
+//         //         code:0,
+//         //         data:result,
+//         //         msg:'商品列表',
+//         //         count:sum
+//         //     });
+//         // });
 
-        user.find().sort({"id":1}).skip((page-1)*limit*1).limit(limit*1).toArray((err,result)=>{
-            // console.log(result);
-            let data;
-            if(err){
-                // console.log(666);
-                data={
-                    'code':1,
-                    'data':[],
-                    'msg':'没有商品',
-                    'count':pages
-                }
-            }else{
-                data = {
-                    'code':0,
-                    'data':result,
-                    'msg':'商品列表',
-                    'count':sum
-                }
+//         user.find().sort({"id":1}).skip((page-1)*limit*1).limit(limit*1).toArray((err,result)=>{
+//             // console.log(result);
+//             let data;
+//             if(err){
+//                 // console.log(666);
+//                 data={
+//                     'code':1,
+//                     'data':[],
+//                     'msg':'没有商品',
+//                     'count':pages
+//                 }
+//             }else{
+//                 data = {
+//                     'code':0,
+//                     'data':result,
+//                     'msg':'商品列表',
+//                     'count':sum
+//                 }
                 
-            }
+//             }
 
-            res.send(data);
-        });
+//             res.send(data);
+//         });
         
         
 
 
-        database.close();
-    })
+//         database.close();
+//     })
 
-})
+// })
 
 //商品列表
 // app.get('/goodslist',function(req,res){
@@ -184,6 +184,108 @@ app.get('/bianji',function(req,res){
 
     });
 });
+
+//点击上下架，改变数据库商品的状态
+app.get('/_change',function(req,res){
+    let change_id = req.query.id*1;
+    let change_stage = req.query.stage;
+    MongoClient.connect('mongodb://localhost:27017',function(err,database){
+        if(err) throw err;
+        let db = database.db('h1809');
+        let user = db.collection('xiangmu');
+        user.update({
+            id:change_id
+        },{$set:{zt:change_stage}},(err,result)=>{
+
+        });
+    });
+});
+//
+
+app.get('/goodscategory',function(req,res){
+    MongoClient.connect('mongodb://localhost:27017',function(err,database){
+        if(err) throw err;
+        let db = database.db('h1809');
+        let user = db.collection('xiangmu');
+        let count = 0;
+
+        let page = req.query.page;
+        let limit = req.query.limit;
+        let key = req.query.key;
+        //console.log()
+        user.find().toArray(function(err2,result2){
+            sum = result2.length;
+            return sum;
+        });
+        // user.find({},{_id:0,class:1}).limit(12).toArray(function(err,result){
+        //     res.send({
+        //         code:0,
+        //         data:result,
+        //         msg:'商品列表',
+        //         count:sum
+        //     });
+        // });
+
+        if(key){
+            user.find({id:(key.id)*1}).toArray((err,result)=>{
+                // console.log(result);
+                let data;
+                if(err){
+                    // console.log(666);
+                    data={
+                        'code':1,
+                        'data':[],
+                        'msg':'没有商品',
+                        'count':pages
+                    }
+                }else{
+                    data = {
+                        'code':0,
+                        'data':result,
+                        'msg':'商品列表',
+                        'count':1
+                    }
+                    
+                }
+
+                res.send(data);
+            });
+        }else{
+            user.find().sort({"id":1}).skip((page-1)*limit*1).limit(limit*1).toArray((err,result)=>{
+                // console.log(result);
+                let data;
+                if(err){
+                    // console.log(666);
+                    data={
+                        'code':1,
+                        'data':[],
+                        'msg':'没有商品',
+                        'count':pages
+                    }
+                }else{
+                    data = {
+                        'code':0,
+                        'data':result,
+                        'msg':'商品列表',
+                        'count':sum
+                    }
+                    
+                }
+
+                res.send(data);
+            });
+        }
+        
+        
+        
+
+
+        database.close();
+    })
+
+})
+
+
 app.listen(3108,function(){
     console.log('server is running on http://localhost:3108')
 });
